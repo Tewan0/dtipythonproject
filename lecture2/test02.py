ee9352f419bd926e140be32bd6b586e4975277c2
Tewan0 <s6652410012@sau.ac.th>
2023-08-22 09:48:18 +0700

    code V.1: Create 4 file.

---
+++ lecture2/test02.py
@@ -0,0 +1,15 @@
+print('Welcome to "SAU"', end='')
+print("Welcome to 'DTI'")
+print("----------------")
+print('Welcome to \'SAU\'', end="")
+print("Welcome to \"DTI\"")
+print("-------------------")
+print(34566)
+print(4547.0545)
+print(True)
+print(False)
+print(15 * 10 + 2) #expression นิพจน์
+print(True or False and False)
+
+
+
